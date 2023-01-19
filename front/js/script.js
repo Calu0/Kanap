05fad045c213fe3067e9e55b7547a2c52a4059ac
Luca K <luca.kanto@gmail.com>
2023-01-19 15:14:13 +0100

    api

--- front/js/script.js
+++ front/js/script.js
@@ -1,1 +1,14 @@
-bblabla
+   
+   fetch("http://localhost:3000/api/products")
+   .then((response) => response.json())
+   
+   
+
+const itemsImg = document.createElement(`#items img`)
+itemsImg.src = products.imageUrl; 
+itemsImg.alt = products.altTxt; 
+const itemsName = document.createElement(`.productName`)
+itemsName.innerText = products.name;
+const itemsDesc = document.createElement(`.productDescription`)
+itemsDesc.innerText = products.description; 
+
