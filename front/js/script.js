1d5b54811d2e2ef7801097311427987a02daec8b
Luca K <luca.kanto@gmail.com>
2023-01-23 11:51:53 +0100

    fetch working

--- front/js/script.js
+++ front/js/script.js
@@ -1,14 +1,8 @@
+ 
+fetch("http://localhost:3000/api/products")
+   .then(products => products.json())
+   .then(function (kanap) {
+      console.log(kanap)
+   }
+   )
    
-   fetch("http://localhost:3000/api/products")
-   .then((response) => response.json())
-   
-   
-
-const itemsImg = document.createElement(`#items img`)
-itemsImg.src = products.imageUrl; 
-itemsImg.alt = products.altTxt; 
-const itemsName = document.createElement(`.productName`)
-itemsName.innerText = products.name;
-const itemsDesc = document.createElement(`.productDescription`)
-itemsDesc.innerText = products.description; 
-
