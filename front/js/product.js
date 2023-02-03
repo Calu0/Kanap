67cfedd83b446ea2b3710e270462fdf7ce46fdcc
Luca K <luca.kanto@gmail.com>
2023-02-03 12:22:58 +0100

    product

--- front/js/product.js
+++ front/js/product.js
@@ -1,12 +1,32 @@
 const getString = window.location.href
 const url = new URL(getString)
 const id = url.searchParams.get("id")
-console.log(id)
+
 
 fetch(`http://localhost:3000/api/products/${id}`)
     .then((response) => response.json())
     .then((data) => getProduct(data))
 
+
 function getProduct(kanap){
-    console.log(kanap.colors)
+    const getImg = kanap.imageUrl
+    const getaltTxt = kanap.altTxt
+    console.log(getImg)
+    showImg(getImg)
+
+
+}
+
+
+function showImg(getImg){
+    const imgProduct = document.createElement(`img`)
+    document.querySelector(`.item__img`).appendChild(imgProduct)
+    imgProduct.src = getImg
+
+}
+
+function showImg(getDesc){
+    const descProduct = document.createElement(``)
+    document.querySelector(`.item__img`).appendChild(descProduct)
+
 }
