1d8247ed16176de22e76a771e6caf2202f160f9e
Luca K <luca.kanto@gmail.com>
2023-02-05 14:54:40 +0100

    product colors oigoing

--- front/js/product.js
+++ front/js/product.js
@@ -9,24 +9,58 @@ fetch(`http://localhost:3000/api/products/${id}`)
 
 
 function getProduct(kanap){
-    const getImg = kanap.imageUrl
-    const getaltTxt = kanap.altTxt
-    console.log(getImg)
-    showImg(getImg)
+    const imgValue = kanap.imageUrl
+    const altValue = kanap.altTxt
+    const descValue = kanap.description
+    const nameValue = kanap.name
+    const priceValue = kanap.price
+    const colorsValue = kanap.colors
 
-
+    showImg(imgValue, altValue)
+    showDesc(descValue)
+    showName(nameValue)
+    showPrice(priceValue)
+    showColors(colorsValue)
+    
 }
 
 
-function showImg(getImg){
+function showImg(imgValue, altValue){
     const imgProduct = document.createElement(`img`)
     document.querySelector(`.item__img`).appendChild(imgProduct)
-    imgProduct.src = getImg
-
+    imgProduct.src = imgValue
+    imgProduct.alt = altValue
 }
 
-function showImg(getDesc){
-    const descProduct = document.createElement(``)
-    document.querySelector(`.item__img`).appendChild(descProduct)
+function showDesc(descValue){
+    const descProduct = document.querySelector(`#description`)
+    descProduct.innerText = descValue
+}
+
+function showName(nameValue){
+    const nameProduct = document.querySelector(`#title`)
+    nameProduct.innerText = nameValue
+}
+
+function showPrice(priceValue){
+    const priceProduct = document.querySelector(`#price`)
+    priceProduct.innerText = priceValue
+}
+
+ const createColors = function(colors){
+        colors = document.createElement(`option`)
+        document.querySelector(`#colors option`).appendChild(colors)
+    
+}
+
+
+
+function showColors(colorsValue){
+
+    for (let i = 0; i <= colorsValue.length; i++){    
+        colors[i]=createColors()
+        colors[i].value = colorsValue[i]
+        console.log(colors)     
+    }
 
 }
