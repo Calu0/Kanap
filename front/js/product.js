143ca2644a0d2c01bb70e8ed62b04042d6a10e13
Luca K <luca.kanto@gmail.com>
2023-02-05 15:36:49 +0100

    colors done product page

--- front/js/product.js
+++ front/js/product.js
@@ -15,52 +15,45 @@ function getProduct(kanap){
     const nameValue = kanap.name
     const priceValue = kanap.price
     const colorsValue = kanap.colors
-
-    showImg(imgValue, altValue)
-    showDesc(descValue)
-    showName(nameValue)
-    showPrice(priceValue)
-    showColors(colorsValue)
     
+    createImg(imgValue, altValue)
+    createDesc(descValue)
+    createName(nameValue)
+    createPrice(priceValue)
+    
+    for(let color of colorsValue){
+        createColors(color)
+    }
 }
 
 
-function showImg(imgValue, altValue){
+function createImg(imgValue, altValue){
     const imgProduct = document.createElement(`img`)
     document.querySelector(`.item__img`).appendChild(imgProduct)
     imgProduct.src = imgValue
     imgProduct.alt = altValue
 }
 
-function showDesc(descValue){
+function createDesc(descValue){
     const descProduct = document.querySelector(`#description`)
     descProduct.innerText = descValue
 }
 
-function showName(nameValue){
+function createName(nameValue){
     const nameProduct = document.querySelector(`#title`)
     nameProduct.innerText = nameValue
 }
 
-function showPrice(priceValue){
+function createPrice(priceValue){
     const priceProduct = document.querySelector(`#price`)
     priceProduct.innerText = priceValue
 }
 
- const createColors = function(colors){
-        colors = document.createElement(`option`)
-        document.querySelector(`#colors option`).appendChild(colors)
-    
+ const createColors = function(color){
+        const colorsProduct = document.createElement(`option`)
+        document.querySelector(`#colors`).appendChild(colorsProduct)
+        colorsProduct.value = color
+        colorsProduct.innerText = color
 }
 
 
-
-function showColors(colorsValue){
-
-    for (let i = 0; i <= colorsValue.length; i++){    
-        colors[i]=createColors()
-        colors[i].value = colorsValue[i]
-        console.log(colors)     
-    }
-
-}
