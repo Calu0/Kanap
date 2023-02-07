df7f794db8949a4bff2b4b57cf8e1fee4856ee1c
Luca K <luca.kanto@gmail.com>
2023-02-07 08:53:50 +0100

    quantity product page done

--- front/js/product.js
+++ front/js/product.js
@@ -8,52 +8,67 @@ fetch(`http://localhost:3000/api/products/${id}`)
     .then((data) => getProduct(data))
 
 
-function getProduct(kanap){
+function getProduct(kanap) {
     const imgValue = kanap.imageUrl
     const altValue = kanap.altTxt
     const descValue = kanap.description
     const nameValue = kanap.name
     const priceValue = kanap.price
     const colorsValue = kanap.colors
-    
+
     createImg(imgValue, altValue)
     createDesc(descValue)
     createName(nameValue)
     createPrice(priceValue)
-    
-    for(let color of colorsValue){
+
+    for (let color of colorsValue) {
         createColors(color)
     }
 }
 
 
-function createImg(imgValue, altValue){
+function createImg(imgValue, altValue) {
     const imgProduct = document.createElement(`img`)
     document.querySelector(`.item__img`).appendChild(imgProduct)
     imgProduct.src = imgValue
     imgProduct.alt = altValue
 }
 
-function createDesc(descValue){
+function createDesc(descValue) {
     const descProduct = document.querySelector(`#description`)
     descProduct.innerText = descValue
 }
 
-function createName(nameValue){
+function createName(nameValue) {
     const nameProduct = document.querySelector(`#title`)
     nameProduct.innerText = nameValue
 }
 
-function createPrice(priceValue){
+function createPrice(priceValue) {
     const priceProduct = document.querySelector(`#price`)
     priceProduct.innerText = priceValue
 }
 
- const createColors = function(color){
-        const colorsProduct = document.createElement(`option`)
-        document.querySelector(`#colors`).appendChild(colorsProduct)
-        colorsProduct.value = color
-        colorsProduct.innerText = color
+const createColors = function (color) {
+    const colorsProduct = document.createElement(`option`)
+    document.querySelector(`#colors`).appendChild(colorsProduct)
+    colorsProduct.value = color
+    colorsProduct.innerText = color
 }
+
+
+function quantity(){
+    const nbr = document.querySelector(`#quantity`)
+    if (nbr.value <= 0 || nbr.value >= 100) {
+      alert(`La quantité séléctionnée est incorrecte. Veuillez choisir un nombre entre 0 et 100.`)
+    }
+
+}
+
+
+document.querySelector(`#addToCart`).addEventListener(`click`, () => quantity())
+
+
+
 
 
