fb439c203c1e404c1819a643708cb926f4fa6c6b
Luca <luca.kanto@gmail.com>
2023-03-05 14:07:34 +0100

    final commit

--- front/js/cart.js
+++ front/js/cart.js
@@ -154,7 +154,6 @@ function showProduct(products) {
 
   quantityTotal()
   priceTotal(products)
-console.log(products)
 }
 
 //fonction qui affiche la quantité totale de produit présent dans le panier
